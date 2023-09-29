1109692a718830db8c3b5976461bdbcce37c1605
cchase <ccchase7@byu.edu>
2023-09-28 21:49:44 -0600

    scrape text into one function

--- scrappy/scrape_utils.py
+++ scrappy/scrape_utils.py
@@ -2,6 +2,11 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 
+from scrape_utils import *
+from utils import *
+from NewsItem import NewsItem
+import json
+
 class ScrapeDriver():
     def __init__(self, headless=False):
         chrome_options = Options()
@@ -24,3 +29,27 @@ class ScrapeDriver():
 
 def valid_news_line(news_line):
     return len(news_line.strip()) > 0
+
+def scrape_page(search_result):
+    output_file = "ynewsItemJson.txt"
+
+    driver = ScrapeDriver(headless=True).driver
+    curr_url = search_result.article_url
+    try:
+        try:
+            driver.get(curr_url)
+        except:
+            pass
+    
+        all_p = driver.find_elements(By.TAG_NAME, 'p')
+
+        curr_news_item = NewsItem(driver.title, search_result, "\n".join([p.text for p in all_p if valid_news_line(p.text)]))
+
+        with open(output_file, "a") as outFile:
+            outFile.write(f"{curr_news_item.toJson()}\n")
+
+    except Exception as e:
+        pass
+
+    finally:
+        driver.quit()
